83162cce9c3917b26c288a81ff1a3e092dd3e9e6
KhanAbdul22 <114879628+KhanAbdul22@users.noreply.github.com>
2023-02-12 21:49:53 -0500

    commit

---
+++ js/app.js
@@ -0,0 +1,43 @@
+import { MY_API_KEY } from './config.js';
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Ready!");
+
+  
+    document.getElementById('buttonCurrency').onclick = getCurrencyExchangeRates;
+
+    
+    
+    getCurrencyExchangeRates();
+    
+});
+
+
+
+
+
+async function getCurrencyExchangeRates()
+{
+    const from = document.getElementById('inputCurrencyFrom').value;
+    const to = document.getElementById('inputCurrencyTo').value;
+    await fetch("https://currency-exchange.p.rapidapi.com/exchange?q=1.0&from=" + from + "&to=" + to, {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "currency-exchange.p.rapidapi.com",
+            "x-rapidapi-key": MY_API_KEY
+        }
+    })
+    .then(response => response.json())
+    .then(response => {
+        console.log("Currency Exchange API object:");
+        console.log(response);
+        console.log("\n");
+
+        // display data
+        document.getElementById('currencyResult').innerHTML = 'Result: ' + response;
+    })
+    .catch(err => {
+        console.log(err);
+    });
+}
+
---
+++ js/config.js
@@ -0,0 +1,2 @@
+const MY_API_KEY = '6b3c53cb76msh56097dbd7ee1842p1c85d7jsn35e1e2adcac0';
+export { MY_API_KEY };
